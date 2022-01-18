89985ae6dd8631056fe22dfd75b7d3811266056a
Navinashri <85227944+Navinashri@users.noreply.github.com>
2022-01-18 21:34:43 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,36 @@
+# boolean
+print(1 > 9)
+print(10 == 10)
+print(10 < 9)
+# floating
+x = float("3.500")
+print(x)
+#int
+x = int(3.5)
+print(x)
+#list
+list1 = ["a", "b", "c"]
+print(list1)
+#tuple
+tuple1 = ("a", "b", "c")
+print(tuple1)
+print(18/4)
+print(18//4)
+print(-18/4)
+print(-18//4)
+y=()
+print(type(y))
+print("Tuple Integer")
+tup2=(10,100,2000,190)
+print(sum(tup2))
+print(min(tup2))
+print(max(tup2))
+print('\n')
+
+tup2=('a','c','t')
+print(min(tup2))
+print(max(tup2))
+#dict
+dict1 = {  "name": "navina",  "age": "100",  "year": 1}
+print(dict1)
+
