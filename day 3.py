1570bb5bc1e0d3dcb6826d57e5ca55cbe792af49
Navinashri <85227944+Navinashri@users.noreply.github.com>
2022-01-19 21:30:48 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,9 @@
+name=input("Enter your Name: ")
+age=int(input("Enter your Age: "))
+gender=input("Enter your Gender: ")
+country=input("Enter your country: ")
+State=input("Enter your state: ")
+ambition=input("What is your ambition in life: ")
+a=[name,age,gender,country,State,ambition]
+print('\n')
+print(f"Name:{a[0]} \nAge:{a[1]} \nGender:{a[2]} \nCountry:{a[3]} \nState:{a[4]}\nAmbition: {a[5]}")
